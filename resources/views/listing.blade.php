6a74676bb8abb1a862ac89ea55dd70fdd330b7f7
Kabeer Hussain <shigristudy@gmail.com>
2021-10-14 19:04:58 +0500

    Https And Text change

--- resources/views/listing.blade.php
+++ resources/views/listing.blade.php
@@ -18,7 +18,20 @@
 
 @push('script')
 <script>
+    function getParameterByName(name, url) {
+        if (!url) url = window.location.href;
+        name = name.replace(/[\[\]]/g, "\\$&");
+        var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+            results = regex.exec(url);
+        if (!results) return null;
+        if (!results[2]) return '';
+        return decodeURIComponent(results[2].replace(/\+/g, " "));
+    }
     $(document).ready(function(){
+        var search = getParameterByName('search',window.location.href);
+        if(search){
+            getJobs(null,search)
+        }
 
         $(document).on('click', '.pagination a', function(event){
             event.preventDefault();
