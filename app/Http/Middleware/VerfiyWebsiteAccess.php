74170fe16d8335adfbbd4205687df3992e494bda
Kabeer Hussain <shigristudy@gmail.com>
2021-12-13 19:39:37 +0500

    Website Active Inactive Option

--- app/Http/Middleware/VerfiyWebsiteAccess.php
+++ app/Http/Middleware/VerfiyWebsiteAccess.php
@@ -23,13 +23,13 @@ class VerfiyWebsiteAccess
             ->where('franchise_slug',request('recruiter'))
             ->first();
         if(!$recruiter){
-            return redirect()->route('not_found');
+            // return redirect()->route('not_found');
         }else{
             $hasWebsite = RecruiterWebsite::where('status','active')
                             ->where('franchise_id',$recruiter->franchise_id)
                             ->first();
             if(!$hasWebsite){
-                return redirect()->route('not_found');
+                // return redirect()->route('not_found');
             }
         }
 
