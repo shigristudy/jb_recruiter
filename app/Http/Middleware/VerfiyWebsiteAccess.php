32d552c68c2cc91eb6aa12f2778f094e0fcbd420
Kabeer Hussain <shigristudy@gmail.com>
2021-12-13 19:10:10 +0500

    Website Active Inactive Option

---
+++ app/Http/Middleware/VerfiyWebsiteAccess.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use App\Models\Recruiter;
+use App\Models\RecruiterWebsite;
+use Closure;
+use Illuminate\Http\Request;
+
+class VerfiyWebsiteAccess
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $recruiter = Recruiter::where('franchise_slug',request('recruiter'))->first();
+        if(!$recruiter){
+            abort(404);
+        }else{
+            $hasWebsite = RecruiterWebsite::where('status','active')->where('franchise_id',$recruiter->franchise_id)->first();
+            if(!$hasWebsite){
+                abort(404);
+            }
+        }
+
+        return $next($request);
+    }
+}
